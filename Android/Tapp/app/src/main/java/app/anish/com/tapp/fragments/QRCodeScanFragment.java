9cc3176551b4fbb149cb65c862f6c177cf7222a0
Anish Khattar <prgrmer25@gmail.com>
2017-04-17 22:28:17 -0700

    Fixed camera display issue

--- Android/Tapp/app/src/main/java/app/anish/com/tapp/fragments/QRCodeScanFragment.java
+++ Android/Tapp/app/src/main/java/app/anish/com/tapp/fragments/QRCodeScanFragment.java
@@ -1,13 +1,20 @@
 package app.anish.com.tapp.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import app.anish.com.tapp.CameraPreview;
@@ -20,6 +27,8 @@ import app.anish.com.tapp.R;
 @SuppressWarnings("deprecation")
 public class QRCodeScanFragment extends Fragment {
 
+    private static final String TAG = QRCodeScanFragment.class.getSimpleName();
+    private static final int MY_PERMISSIONS_REQUEST_FOR_CAMERA = 1;
 
     private final PreviewCallback previewCallback = new PreviewCallback() {
         @Override
@@ -30,33 +39,75 @@ public class QRCodeScanFragment extends Fragment {
 
     private Camera mCamera;
     private CameraPreview mCameraPreview;
+    private Button reqCamPermButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.qr_code_scan, container, false);
-        initializeCamera();
-        initUI(rootView);
+        reqCamPermButton = (Button) rootView.findViewById(R.id.bReqCameraPerm);
+        checkForPermissionAndInitCamera();
+        initCameraUI(rootView);
         return rootView;
     }
 
     private void initializeCamera() {
         mCamera = getCameraInstance();
-        mCameraPreview = new CameraPreview(getContext(),mCamera, previewCallback);
+        if (mCamera != null) {
+            mCameraPreview = new CameraPreview(getContext(), mCamera, previewCallback);
+            reqCamPermButton.setVisibility(View.GONE);
+            reqCamPermButton.setEnabled(false);
+        }
     }
 
-    private void initUI(View rootView) {
-        FrameLayout frameLayout = (FrameLayout) rootView.findViewById(R.id.flCamera);
-        frameLayout.addView(mCameraPreview);
+    private void initReqPermButton(View rootView) {
+        reqCamPermButton = (Button) rootView.findViewById(R.id.bReqCameraPerm);
+        reqCamPermButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkForPermissionAndInitCamera();
+            }
+        });
+
+    }
+
+    private void initCameraUI(View rootView) {
+        if (mCameraPreview != null) {
+            FrameLayout frameLayout = (FrameLayout) rootView.findViewById(R.id.flCamera);
+            frameLayout.addView(mCameraPreview);
+        }
     }
 
     private static Camera getCameraInstance() {
         try {
             return Camera.open();
         } catch (Exception e) {
+            Log.d(TAG,"Error getting camera instance", e);
             return null;
         }
     }
 
 
+    private void checkForPermissionAndInitCamera() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_FOR_CAMERA);
+        } else {
+            initializeCamera();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FOR_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Camera Permission granted
+                    initializeCamera();
+                } else {
+                    reqCamPermButton.setVisibility(View.VISIBLE);
+                    reqCamPermButton.setEnabled(true);
+                }
+        }
+    }
 }
