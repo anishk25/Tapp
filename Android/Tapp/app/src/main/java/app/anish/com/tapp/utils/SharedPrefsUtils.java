5fabe67a9445938f30762fca927467808d5ac04c
Khattar <akhattar@amazon.com>
2017-04-24 20:07:02 -0700

    added SharedPrefsUtils

---
+++ Android/Tapp/app/src/main/java/app/anish/com/tapp/utils/SharedPrefsUtils.java
@@ -0,0 +1,25 @@
+package app.anish.com.tapp.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Util class for saving to and reading date from
+ * {@link SharedPreferences}
+ * Created by akhattar on 4/24/17.
+ */
+
+public final class SharedPrefsUtils {
+
+    public static String getString(Context context, String prefsKey, String valueKey) {
+        SharedPreferences sharedPref = context.getSharedPreferences(prefsKey, Context.MODE_PRIVATE);
+        return sharedPref.getString(valueKey, null);
+    }
+
+    public static void saveString(Context context, String prefsKey, String valueKey, String value) {
+        SharedPreferences sharedPref = context.getSharedPreferences(prefsKey, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(valueKey, value);
+        editor.commit();
+    }
+}
