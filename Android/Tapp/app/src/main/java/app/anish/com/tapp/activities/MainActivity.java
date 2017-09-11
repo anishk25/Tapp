da8ce1ff88b05166f1822ddf4bed8baae53ed3c7
Anish Khattar <prgrmer25@gmail.com>
2017-09-10 17:06:28 -0700

    added screen overlay permission request

--- Android/Tapp/app/src/main/java/app/anish/com/tapp/activities/MainActivity.java
+++ Android/Tapp/app/src/main/java/app/anish/com/tapp/activities/MainActivity.java
@@ -1,11 +1,16 @@
 package app.anish.com.tapp.activities;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import app.anish.com.tapp.R;
 import app.anish.com.tapp.adapters.SectionsPagerAdapter;
@@ -13,6 +18,8 @@ import app.anish.com.tapp.adapters.SectionsPagerAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private static final int PERM_REQUEST_CODE_DRAW_OVERLAYS = 1;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -27,17 +34,9 @@ public class MainActivity extends AppCompatActivity {
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        requestDrawOverlaysPermission();
+        setupViewPager();
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.vpContainer);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.addOnPageChangeListener(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
     }
 
     @Override
@@ -54,11 +53,46 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager() {
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.vpContainer);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.addOnPageChangeListener(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    /**
+     * this permission is needed to to remove the "Screen Overlay Detected" dialog
+     * The dialog causes errors in requesting permissions.
+     */
+    public void requestDrawOverlaysPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, PERM_REQUEST_CODE_DRAW_OVERLAYS);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PERM_REQUEST_CODE_DRAW_OVERLAYS && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !Settings.canDrawOverlays(this)) {
+            Toast.makeText(this, "Screen Overlay Permission not granted. This will most likely cause " +
+                    "issues with granting permissions", Toast.LENGTH_LONG).show();
+        }
     }
 }
